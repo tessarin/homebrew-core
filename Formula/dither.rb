1df1643b1bced8a162831ccdef70f84f205f8203
Cesar Tessarin <cesar.tessarin@gmail.com>
2017-10-04 22:54:33 -0300

    Update dither

--- Formula/dither.rb
+++ Formula/dither.rb
@@ -1,8 +1,10 @@
 class Dither < Formula
   desc "Image utility for color quantization and dithering."
   url "https://github.com/tessarin/dither/archive/v0.2.tar.gz"
-  sha256 "b33a3fc6b9bccb17137301bdb9c2a508544ae283127dabbc6ee9a810e4f1bbb7"
+  sha256 "0e233ec7acd52d265596aef69b191c8a10d28e90a5787522093e668c6a6eff75"
   head "https://github.com/tessarin/dither.git"
+
+  depends_on "libpng"
 
   def install
     system "make"
